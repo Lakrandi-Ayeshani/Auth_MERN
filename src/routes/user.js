5d540d98ede9d3a80a98ef4eeb1837f73900348a
Lakrandi-Ayeshani <Lakrandiayeshani1204@gmail.com>
2023-09-10 13:19:54 +0530

    routes protected middleware developed. user profile get/update and logout end points developed

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile } = require("../controllers/user.js");
+const { protect } = require('../middleware/authMidleware.js');
 
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
 // router.get('/profile', getUserProfile);
 // router.put('/profile', updateUserProfile);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router.route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
 
 module.exports = { userRouter: router }
--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cookieParser = require('cookie-parser');
 const { userRouter } = require("./routes/user");
 const { notFound, errorHandler } = require('./middleware/errorMiddleware.js');
 const { connectDB } = require("./config/db");
 dotenv.config();
 const PORT = process.env.PORT;
 
+connectDB();
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-connectDB();
+app.use(cookieParser());
 
 app.get("/", (req, res) => res.send("Server is running correctly"));
 app.use('/api/user', userRouter)
